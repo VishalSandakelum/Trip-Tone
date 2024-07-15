f65afa0bedb1cb28508c6fa6677ec6bf5f56b993
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-07-15 13:51:35 +0530

    add image for readme file

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/login">View details</Link>
     </View>
   );
 }
